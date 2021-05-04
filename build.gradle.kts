e723d2c4d1aa9b4522a135391d344653a6bd2caa
GitHub Action <action@github.com>
2021-05-04 22:44:29 +0000

    Setup using android-template - by https://github.com/omarmiatello/start-android

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    kotlin("android") apply false
+    id("io.gitlab.arturbosch.detekt") version "1.16.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("com.github.ben-manes.versions") version "0.38.0"
+}
+
+allprojects {
+    group = "com.github.omarmiatello.source-of-truth"
+    repositories {
+        mavenCentral()
+        google()
+        jcenter()   // JCenter is at end of life
+    }
+}
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("com.github.ben-manes.versions")
+    }
+
+    ktlint {
+        debug.set(false)
+        version.set(Version.ktlint)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        enableExperimentalRules.set(true)
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
+
+    detekt {
+        config = rootProject.files("config/detekt/detekt.yml")
+        reports {
+            html {
+                enabled = true
+                destination = file("build/reports/detekt.html")
+            }
+        }
+    }
+}
+
+tasks {
+    register("clean", Delete::class.java) {
+        delete(rootProject.buildDir)
+    }
+
+    withType<DependencyUpdatesTask> {
+        rejectVersionIf {
+            candidate.version.isStableVersion().not()
+        }
+    }
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,89 @@
+fun String.isStableVersion(): Boolean {
+    val upperCase = toUpperCase(java.util.Locale.ROOT)
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { it in upperCase }
+    return stableKeyword || Regex("^[0-9,.v-]+(-r)?$").matches(this)
+}
+
+fun String.isNotStableVersion() = !isStableVersion()
+
+object Sdk {
+    const val min = 21
+    const val target = 30
+    const val compile = 30
+}
+
+object Version {
+    const val java = "11"
+    const val dokka = "1.4.20"
+    const val ktlint = "0.41.0"
+}
+
+object Lib {
+    const val androidxActivityKtx = "androidx.activity:activity-ktx:1.3.0-alpha07"
+    const val androidxAppcompat = "androidx.appcompat:appcompat:1.3.0-rc01"
+    const val androidxCameraCamera2 = "androidx.camera:camera-camera2:1.1.0-alpha04"
+    const val androidxCameraCore = "androidx.camera:camera-core:1.1.0-alpha04"
+    const val androidxCameraExtensions = "androidx.camera:camera-extensions:1.0.0-alpha24"
+    const val androidxCameraLifecycle = "androidx.camera:camera-lifecycle:1.1.0-alpha04"
+    const val androidxCameraView = "androidx.camera:camera-view:1.0.0-alpha24"
+    const val androidxConstraintlayout = "androidx.constraintlayout:constraintlayout:2.1.0-beta01"
+    const val androidxCoreKtx = "androidx.core:core-ktx:1.6.0-alpha02"
+    const val androidxFragmentKtx = "androidx.fragment:fragment-ktx:1.3.3"
+    const val androidxLifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01"
+    const val androidxLivedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:2.4.0-alpha01"
+    const val androidxNavigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:2.3.5"
+    const val androidxNavigationUiKtx = "androidx.navigation:navigation-ui-ktx:2.3.5"
+    const val androidxRecyclerview = "androidx.recyclerview:recyclerview:1.2.0"
+    const val androidxViewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0-alpha01"
+    const val composeAccompanistCoil = "com.google.accompanist:accompanist-coil:0.8.1"
+    const val composeActivity = "androidx.activity:activity-compose:1.3.0-alpha07"
+    const val composeFoundation = "androidx.compose.foundation:foundation:1.0.0-beta05"
+    const val composeIconsCore = "androidx.compose.material:material-icons-core:1.0.0-beta05"
+    const val composeIconsExt = "androidx.compose.material:material-icons-extended:1.0.0-beta05"
+    const val composeMaterial = "androidx.compose.material:material:1.0.0-beta05"
+    const val composeUi = "androidx.compose.ui:ui:1.0.0-beta05"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:1.0.0-beta05"
+    const val composeViewmodel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04"
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0-RC"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC"
+    const val coroutinesPlayServices = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.0-RC"
+    const val dagger = "com.google.dagger:dagger:2.35.1"
+    const val firebaseDatabaseKtx = "com.google.firebase:firebase-database-ktx:19.7.0"
+    const val firebaseStorageKtx = "com.google.firebase:firebase-storage-ktx:19.2.2"
+    const val googleMaterial = "com.google.android.material:material:1.4.0-alpha02"
+    const val hiltAndroid = "com.google.dagger:hilt-android:2.35.1"
+    const val hiltCommon = "androidx.hilt:hilt-common:1.0.0"
+    const val hiltViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0"
+    const val kaptDaggerCompiler = "com.google.dagger:dagger-compiler:2.35.1"
+    const val kaptHiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:2.35.1"
+    const val kaptHiltCompiler = "androidx.hilt:hilt-compiler:1.0.0"
+    const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:1.5.0"
+    const val ktorClientOkhttp = "io.ktor:ktor-client-okhttp:1.5.4"
+    const val mlkitBarcodeScanning = "com.google.mlkit:barcode-scanning:16.1.1"
+    const val mlkitTextRecognition = "com.google.android.gms:play-services-mlkit-text-recognition:16.1.3"
+    const val testAndroidxEspresso = "androidx.test.espresso:espresso-core:3.4.0-alpha06"
+    const val testAndroidxExtJunit = "androidx.test.ext:junit:1.1.3-alpha06"
+    const val testAndroidxRules = "androidx.test:rules:1.4.0-alpha06"
+    const val testAndroidxRunner = "androidx.test:runner:1.4.0-alpha06"
+    const val testJunit = "junit:junit:4.13.2"
+}
+
+val String.version get() = substringAfterLast(":")
+
+object LibGroup {
+    private val composeBasic = listOf(
+        Lib.composeUi, Lib.composeFoundation, Lib.composeMaterial, Lib.composeActivity
+    )
+    private val composeIconsViewModelCoil = listOf(
+        Lib.composeIconsCore, Lib.composeIconsExt, Lib.composeViewmodel,
+        Lib.composeAccompanistCoil
+    )
+    val composeAll = composeBasic + composeIconsViewModelCoil
+    val composeDebug = listOf(Lib.composeUiTooling, Lib.kotlinReflect)
+
+    val testAndroid = listOf(
+        Lib.testAndroidxRules, Lib.testAndroidxExtJunit, Lib.testAndroidxEspresso
+    )
+}
+
+// Project modules
